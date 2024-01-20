0cf0c984043814d9c1557e2838406f0db7e0e851
nikitazapekin <wotblitz362@mail.ru>
2024-01-20 18:20:46 +0300

    display of results

--- src/hooks/useCalculate.ts
+++ src/hooks/useCalculate.ts
@@ -1,79 +1,6 @@
 
 
 import { useEffect, useState } from "react";
-/*
-class CalculatorOperations {
-  arrayOfBracketEcpression: String[]=[]
-  arrayOfBracketNumbers: String[]=[]
-  static operation(subExpression: string){
-const regex = /([-+/])/g;
-const tokens = subExpression.split(regex).filter(Boolean);
-console.log("TOKENS" +tokens)
-let i = 1;
-while (i < tokens.length) {
-  if (tokens[i] === '+') {
-    const sum = Number(tokens[i - 1]) + Number(tokens[i + 1]);
-    tokens.splice(i - 1, 3, String(sum));
-    i -= 2; 
-  }
-  if (tokens[i] === '-') {
-    const sum = Number(tokens[i - 1]) - Number(tokens[i + 1]);
-    tokens.splice(i - 1, 3, String(sum));
-    i -= 2; 
-  }
-  i += 2; 
-}
-console.log("Modified TOKENS: " + tokens);
-return tokens.join('')
-
-  } 
- static calculatePartOfExpression(expression: string, operation: string) {
-  let indexOfLastPlus = expression.lastIndexOf(operation)
-console.log("EXPRESS"+expression)
-let  arrayOfPriorityEcpression: String[]=[]
-let isBracket: boolean  = false
-let regex = /(\d+|\+|\(|\))/g;
-let result = expression.match(regex);
-console.log(result);
-let countOfBrackets=0;
-
-console.log("RES" +result)
-let indexesOfLeftBrackets: Number[]= []
-let indexesOfRightBrackets: Number[]= []
- 
-result.forEach((item, index)=> {
-  let expressionInBracket;
-  if(item=="("){
-    countOfBrackets++
-    indexesOfLeftBrackets.push(index)
-    for(let i=index; i<result.length; i++){
-if(result[i]=='(' )  {
-  countOfBrackets++
-}
-if(result[i]==')'){
-  countOfBrackets--
-}
-if(countOfBrackets==0){
-  console.log("SCOPE"+result.splice(index, i))
-}
-    }
-  }
-
-  if(item==")"){
-     indexesOfRightBrackets.push(index)
-   }
-
-  })
-  console.log(indexesOfLeftBrackets)
-console.log(indexesOfRightBrackets)
-
-
-  
-}
-} */
-
-
-
 function control(testString: string) { 
   testString = testString.replace(/\++/g, "+");
   testString = testString.replace(/\--/g, "+");
@@ -148,9 +75,8 @@ function calculation(buffString: string) {
      stringId++;
      polishString[stringId] = polishStack[stackId];
      stackId--;
- }                                                                  // польская запись готова
-//   alert(polishString);
- stackId = -1;                                                      // Начинаем считать по польской записи
+ }                                                                 
+ stackId = -1;               
  var stringIdMax = stringId;
 
  for (stringId = 0; stringId <= stringIdMax; stringId++ ) {
@@ -178,10 +104,6 @@ function calculation(buffString: string) {
  }
  return polishStack[stackId];
 }
-
-
-
-
 export class Calculator {
   currentExpression: string = "";
   currentResult: number = 0;
@@ -193,10 +115,14 @@ setExpression(symbol: string) {
   this.currentExpression += symbol;
 }
 if(this.currentExpression.length==0) {
-  if( !['+', '-', '*', '/', '=', 'C', '✖'].includes(symbol)){
+  if( !['+', '*', '/', '=', 'C', '✖'].includes(symbol)){
     this.currentExpression += symbol;
     
   }
+}
+if(this.currentExpression.length>0 && symbol=="=") {
+this.currentExpression=this.currentExpression+calculation(this.currentExpression.substring(0, this.currentExpression.length-1))
+//this.currentExpression
 }
   } 
    
@@ -212,8 +138,6 @@ const [currentExpression, setCurrentExpression] = useState<string>("");
  useEffect(()=> {
 calculator.setExpression(selectedSymbol)
 setCurrentExpression(calculator.currentExpression)
-//switch(selectedSymbol){
- 
  calculator.sumAction()  
  console.log("RESULT" +calculator.currentResult)
  }, [selectedSymbol])
